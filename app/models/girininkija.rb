f96b47f5132040a5646c092413be53f83eba7969
RokasPovilionis <rokcep@gmail.com>
2021-12-16 00:05:19 +0200

    populate girininkijos

---
+++ app/models/girininkija.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Model for Girininkijos
+class Girininkija < ApplicationRecord
+  self.table_name = 'girininkijos'
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,19 +9,34 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Uredija.delete_all
+Girininkija.delete_all
+
+connection = ActiveRecord::Base.connection()
 
 if Uredija.all.count.zero?
-  connection = ActiveRecord::Base.connection()
 
   # Import uredija data from shpfile to uredijos table
   from_uredijos_shp_sql =
     `shp2pgsql -c -g geom -W LATIN1 -s 4326 #{Rails.root.join('db', 'shpfiles', 'Uredija.shp')} uredijos_ref`
   connection.execute 'drop table if exists uredijos_ref'
   connection.execute from_uredijos_shp_sql
-  # binding.pry
   connection.execute <<-SQL
       insert into uredijos(mu_kod, pavadinimas, geom, created_at, updated_at)
         select mu_kod, pavadinima, geom, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP from uredijos_ref
   SQL
   connection.execute 'drop table uredijos_ref'
+end
+
+if Girininkija.all.count.zero?
+
+  # Import uredija data from shpfile to uredijos table
+  from_girininkijos_shp_sql =
+    `shp2pgsql -c -g geom -W LATIN1 -s 4326 #{Rails.root.join('db', 'shpfiles', 'Girininkija.shp')} girininkijos_ref`
+  connection.execute 'drop table if exists girininkijos_ref'
+  connection.execute from_girininkijos_shp_sql
+  connection.execute <<-SQL
+    insert into girininkijos(mu_kod, gir_kod, pavadinimas, geom, created_at, updated_at)
+      select mu_kod, gir_kod, pavadinima, geom, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP from girininkijos_ref
+  SQL
+  connection.execute 'drop table girininkijos_ref'
 end
