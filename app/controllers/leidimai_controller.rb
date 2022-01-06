5e81665df92251818be2be9daa8fc1d10825a732
RokasPovilionis <rokcep@gmail.com>
2022-01-06 06:10:09 +0200

    add create cutting permit page

--- app/controllers/leidimai_controller.rb
+++ app/controllers/leidimai_controller.rb
@@ -2,9 +2,32 @@
 
 # Controller for leidimai screen
 class LeidimaiController < ApplicationController
-  skip_before_action :authorized
+  skip_before_action :authorized, only: [:index]
 
   def index
     @leidimai = Leidimas.paginate(page: params[:page], per_page: 30)
+  end
+
+  def new
+    @leidimas = Leidimas.new
+  end
+
+  def create
+    @leidimas = Leidimas.new(leidimas_params)
+
+    if @leidimas.save
+      redirect_to leidimai_index_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def leidimas_params
+    params.require(:leidimas).permit(:serija_ir_nr, :regionas, :rajonas, :nuosavybes_forma, :uredija,
+                                     :girininkija, :kvartalas, :sklypai, :plotas, :kad_vietove,
+                                     :kad_blokas, :kad_nr, :kirtimo_rusis, :galiojimo_pradzia,
+                                     :galiojimo_pabaiga)
   end
 end
