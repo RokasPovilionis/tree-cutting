05174157aba269488657ca14c6d44af21f4eca6a
Rokas Povilionis <rokcep@gmail.com>
2023-01-01 19:53:37 +0200

    Remove duplication of dinamically inserted STK table seeding

--- db/seeds/valstybes_saugomu_teritoriju_kadastras.rb
+++ db/seeds/valstybes_saugomu_teritoriju_kadastras.rb
@@ -11,8 +11,35 @@ MANUALLY_INSERTED_STK_TABLES = [
     file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Geologiniai_GPO_Task.shp" }
 ].freeze
 
+DINAMICALLY_INSERTED_STK_TABLES = [
+  { class: HidrografiniaiGpoPlotiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Hidrografiniai_GPO_Plot.shp" },
+  { class: GeomorfologiniaiGpoPlotiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Geomorfologiniai_GPO_Plot.shp" },
+  { class: HidrogeologiniaiGpoPlotiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Hidrogeologiniai_GPO_Plot.shp" },
+  { class: BotaniniaiGpoPlotiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Botaniniai_GPO_Plot.shp" },
+  { class: BotaniniaiGpoTaskiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Botaniniai_GPO_Task.shp" },
+  { class: ZoologiniaiGpoPlotiniai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Zoologiniai_GPO_Plot.shp" },
+  { class: StkSklypai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Sklypai.shp" },
+  { class: StkParkai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Parkai.shp" },
+  { class: BiosferosRezervatai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BiosferosRezervatai.shp" },
+  { class: BiosferosPoligonai,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BiosferosPoligonai.shp" },
+  { class: FunkcinioPrioritetoZonos,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/FunkcinioPrioritetoZonos.shp" },
+  { class: BuferinesApsaugosZonos,
+    file_location: "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BuferinesApsaugosZonos.shp" }
+].freeze
+
 MANUALLY_INSERTED_STK_TABLES.each do |table_hash|
-  table_hash[:class].count.zero?
+  next unless table_hash[:class].count.zero?
 
   puts "seeding seeding STK #{table_hash[:class]}"
 
@@ -21,146 +48,12 @@ MANUALLY_INSERTED_STK_TABLES.each do |table_hash|
   puts 'Great Success!'
 end
 
-if HidrografiniaiGpoPlotiniai.count.zero?
-  puts 'seeding STK Hidrografiniai gamtos paveldo objektai plotiniai'
+DINAMICALLY_INSERTED_STK_TABLES.each do |table_hash|
+  next unless table_hash[:class].count.zero?
 
-  ReadShapeFiles.run(
-    HidrografiniaiGpoPlotiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Hidrografiniai_GPO_Plot.shp",
-    false
-  )
+  puts "seeding seeding STK #{table_hash[:class]}"
 
-  puts 'Great Success!'
-end
-
-if GeomorfologiniaiGpoPlotiniai.count.zero?
-  puts 'seeding STK Geomorfologiniai gamtos paveldo objektai plotiniai'
-
-  ReadShapeFiles.run(
-    GeomorfologiniaiGpoPlotiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Geomorfologiniai_GPO_Plot.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if HidrogeologiniaiGpoPlotiniai.count.zero?
-  puts 'seeding STK Hidrogeologiniai gamtos paveldo objektai plotiniai'
-
-  ReadShapeFiles.run(
-    HidrogeologiniaiGpoPlotiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Hidrogeologiniai_GPO_Plot.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if BotaniniaiGpoPlotiniai.count.zero?
-  puts 'seeding STK Botaniniai gamtos paveldo objektai plotiniai'
-
-  ReadShapeFiles.run(
-    BotaniniaiGpoPlotiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Botaniniai_GPO_Plot.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if BotaniniaiGpoTaskiniai.count.zero?
-  puts 'seeding STK Botaniniai gamtos paveldo objektai taskiniai'
-
-  ReadShapeFiles.run(
-    BotaniniaiGpoTaskiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Botaniniai_GPO_Task.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if ZoologiniaiGpoPlotiniai.count.zero?
-  puts 'seeding STK Zoologiniai gamtos paveldo objektai plotiniai'
-
-  ReadShapeFiles.run(
-    ZoologiniaiGpoPlotiniai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Zoologiniai_GPO_Plot.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if StkSklypai.count.zero?
-  puts 'seeding STK sklypai'
-
-  ReadShapeFiles.run(
-    StkSklypai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Sklypai.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if StkParkai.count.zero?
-  puts 'seeding STK Parkai'
-
-  ReadShapeFiles.run(
-    StkParkai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/Parkai.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if BiosferosRezervatai.count.zero?
-  puts 'seeding STK Biosferos Rezervatai'
-
-  ReadShapeFiles.run(
-    BiosferosRezervatai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BiosferosRezervatai.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if BiosferosPoligonai.count.zero?
-  puts 'seeding STK Biosferos Poligonai'
-
-  ReadShapeFiles.run(
-    BiosferosPoligonai,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BiosferosPoligonai.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if FunkcinioPrioritetoZonos.count.zero?
-  puts 'seeding STK Funkcinio Prioriteto Zonos'
-
-  ReadShapeFiles.run(
-    FunkcinioPrioritetoZonos,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/FunkcinioPrioritetoZonos.shp",
-    false
-  )
-
-  puts 'Great Success!'
-end
-
-if BuferinesApsaugosZonos.count.zero?
-  puts 'seeding STK Buferines Apsaugos Zonos'
-
-  ReadShapeFiles.run(
-    BuferinesApsaugosZonos,
-    "#{Rails.root}/db/valstybes_saugomu_teritoriju_kadastras/BuferinesApsaugosZonos.shp",
-    false
-  )
+  ReadShapeFiles.run(table_hash[:class], table_hash[:file_location], false)
 
   puts 'Great Success!'
 end
