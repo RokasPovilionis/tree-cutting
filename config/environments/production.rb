35c63a65d38c984c927c7b4d7e250acbee70c47b
Rokas Povilionis <rokcep@gmail.com>
2024-01-08 04:44:01 +0200

    fix typo in config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'treecuttingpermits@gmail.com>',
+    user_name: 'treecuttingpermits@gmail.com',
     password: ENV.fetch('GOOGLE_APP_PASSWORD'),
     authentication: 'plain',
     enable_starttls_auto: true
